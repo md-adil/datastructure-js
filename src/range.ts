cbaa7e6749e784e13ec6f939ed6396cbee4aca4d
Md Adil <md-adil@live.com>
2025-05-16 00:16:32 +0530

    fix range

--- src/range.ts
+++ src/range.ts
@@ -1,17 +1,29 @@
 type Callback<T, U> = (value: T, index: number, instance: Range) => U;
 class Range {
   public readonly end: number;
-  constructor(
-    public readonly start: number,
-    end?: number,
-    public readonly steps = 1
-  ) {
-    if (!end) {
+  constructor(public readonly start: number, end?: number, public readonly steps = 1) {
+    if (end === undefined) {
       this.end = start;
       this.start = 0;
     } else {
       this.end = end;
     }
+    if (steps === 0) {
+      throw new Error("Step cannot be zero");
+    }
+    if ((this.start < this.end && steps < 0) || (this.start > this.end && steps > 0)) {
+      throw new Error("Step direction incompatible with range direction");
+    }
+  }
+
+  get length(): number {
+    return Math.max(0, Math.ceil(Math.abs(this.end - this.start) / Math.abs(this.steps)));
+  }
+
+  at(index: number): number | undefined {
+    const len = this.length;
+    if (index < 0 || index >= len) return undefined;
+    return this.start + index * this.steps;
   }
 
   map<U>(callback: Callback<number, U>) {
@@ -36,9 +48,16 @@ class Range {
     }
   }
 
+  reverse() {
+    return new Range(this.end - this.steps, this.start - Math.sign(this.steps), -this.steps);
+  }
+
   *[Symbol.iterator]() {
     let start = this.start;
-    while (Math.abs(start - this.end) > 0) {
+    const steps = this.steps;
+    const end = this.end;
+
+    while (steps > 0 ? start < end : start > end) {
       yield start;
       start += this.steps;
     }
